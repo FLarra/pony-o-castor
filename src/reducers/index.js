1befb5cb7c9d584440fdf5dfb85fd16221596163
Fabian Larranaga <ffabian.larranaga@gmail.com>
2019-02-17 16:42:20 -0300

    [4] Add ponify employee reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,15 @@ const addEmployee = (state = [], action) => {
       return [
         ...state,
         {
-          name: action.name
+          name: action.name,
+          pony: true
         }
       ];
+    case 'PONIFY_EMPLOYEE':
+      return state.map(employee =>
+        (employee.name === action.name) ? {...employee, pony: !employee.pony} : employee
+      )
+
     default:
       return state;
   }
